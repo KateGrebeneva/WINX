7d1b90f8f851044ffb8a1cedabb91259a2f403e0
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-11-21 17:15:18 +0300

    winx_7.2

--- testMongoose2024.js
+++ testMongoose2024.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
-const Winx = mongoose.model('Winx', { name: String });
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.magic = function(){
+    console.log(this.name + " сказала магия!")
+}
+
+const Winx = mongoose.model('Winx', schema);
+
 const fairy = new Winx({ name: 'Рокси' });
-fairy.save().then(() => console.log('Магия!'));
+fairy.save().then(() => fairy.magic());
