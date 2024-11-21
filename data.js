186fe0d4f7e7539613ed5fd1e07ae9921201ca8e
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2024-11-21 16:58:30 +0300

    winx_6.2

---
+++ data.js
@@ -0,0 +1,41 @@
+var data = [{
+    title: 'Блум',
+    nick: 'bloom',
+    avatar: '/images/bloom.png',
+    desc: 'Фея Огня Дракона и принцесса планеты Домино.'
+    },
+    {
+        title: 'Стелла',
+        nick: 'stella',
+        avatar: '/images/stella.png',
+        desc: 'Фея Солнца, Луны и Звёзд и принцесса Солярии.'
+        },
+    { 
+        title: 'Флора',
+        nick: 'flora',
+        avatar: '/images/flora.png',
+        desc: 'Фея природы и фея-хранительница Линфеи.'
+        },
+    {
+        title: 'Муза',
+        nick: 'musa',
+        avatar: '/images/musa.png',
+        desc: 'Фея музыки и гармонии, хранительница Мелодии.'
+        },
+    {
+        title: 'Текна',
+        nick: 'tecna',
+        avatar: '/images/tecna.png',
+        desc: 'Фея технологий и фея-хранительница Зенита.'
+            },
+    { 
+        title: 'Лейла',
+        nick: 'aisha',
+        avatar: '/images/aisha.png',
+        desc: 'Фея волн и морфикса, принцесса Андроса.'
+            }
+    ];
+    
+    module.exports.data = data;
+
+    
